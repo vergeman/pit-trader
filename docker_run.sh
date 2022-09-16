76020bc1b5b95bc2ebe31166b12f60d74a12eb7c
vergeman <alan.verga@gmail.com>
2022-09-16 15:22:28 -0500

    initial Dockerized setup for webcam, notebook, mediapipe

---
+++ docker_run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+# did you remember
+# xhost +local:docker
+
+#docker run --user root --device /dev/video0:/dev/video0 \
+#       -e GRANT_SUDO=yes -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
+#       -v /home/vergeman/dev/pit-trader/data:/home/jovyan/data \
+#       -it pit-trader_pytorch-minimal-notebook python data/test_webcam.py
+
+# run as exec lighter?
+
+docker exec -e DISPLAY=$DISPLAY -it pit-trader_pytorch-minimal-notebook_1 \
+       python data/test_webcam.py
+
