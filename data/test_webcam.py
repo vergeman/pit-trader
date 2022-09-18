e8aff7540ed6005ec1a69c02440bad7b59908deb
vergeman <alan.verga@gmail.com>
2022-09-18 00:18:07 -0500

    insert mediapipe face_detection display

--- data/test_webcam.py
+++ data/test_webcam.py
@@ -11,7 +11,7 @@ import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
-
+mp_face_detection = mp.solutions.face_detection
 
 # For static images:
 # IMAGE_FILES = []
@@ -58,7 +58,9 @@ mp_hands = mp.solutions.hands
 with mp_hands.Hands(
     model_complexity=0,
     min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
+    min_tracking_confidence=0.5) as hands, mp_face_detection.FaceDetection(
+    model_selection=0, min_detection_confidence=0.5) as face_detection:
+
   while cap.isOpened():
     success, image = cap.read()
     if not success:
@@ -70,13 +72,15 @@ with mp_hands.Hands(
     # pass by reference.
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
+    resultsHands = hands.process(image)
+    resultsFace = face_detection.process(image)
 
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
+
+    if resultsHands.multi_hand_landmarks:
+      for hand_landmarks in resultsHands.multi_hand_landmarks:
 
         for i, landmark in enumerate(hand_landmarks.landmark):
           print(i, [landmark.x, landmark.y, landmark.z])
@@ -91,6 +95,11 @@ with mp_hands.Hands(
             mp_drawing_styles.get_default_hand_landmarks_style(),
             mp_drawing_styles.get_default_hand_connections_style())
 
+    # draw face detection
+    if resultsFace.detections:
+      for detection in resultsFace.detections:
+        mp_drawing.draw_detection(image, detection)
+
     # resize input
     # Flip the image horizontally for a selfie-view display.
 
@@ -98,8 +107,10 @@ with mp_hands.Hands(
     # image = cv2.resize(image, (800, 400))
     image = cv2.flip(image, 1)
 
-    cv2.imshow('MediaPipe Hands', image)
+    cv2.imshow('MediaPipe Hands + Face', image)
 
     if cv2.waitKey(5) & 0xFF == 27:
       break
+
+
 cap.release()
