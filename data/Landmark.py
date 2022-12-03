d27a2b8f5ae5e9e21ee5551a13bdd48d9bd0db80
Alan Verga <alan.verga@gmail.com>
2022-12-03 17:44:22 -0600

    Topic/refined landmarks (#3)
    * refactor hand_landmarks to discrete left_hand / right_hand - remove handedness calc
    
    * add Landmark:setBasePoints() to reorient on selected points (x,y)
    
    * move comments / notes out to README.md
    
    * rename test_webcam.py -> webcam_trainer.py
    
    * move relativize toggle to Landmark:setBasepoints()
    
    * remove width/height from landmark to_row() output

--- data/Landmark.py
+++ data/Landmark.py
@@ -2,14 +2,19 @@
 # then build concatenates those
 # we can test what seems to be better for a classifier
 # e.g. each type of data -- doesn't it all get ocncatenated anyway?
-
+import mediapipe as mp
+mp_face_detection = mp.solutions.face_detection
 
 class Landmark:
 
     def __init__(self):
-        self.handedness = []
-        self.hand_landmarks = [ [], [] ]
+        self.left_hand_landmarks = []
+        self.right_hand_landmarks = []
         self.face_landmarks = []
+
+        # relative points
+        self.base_x = 0
+        self.base_y = 0
 
         # default opencv width height
         self.width = 640
@@ -22,104 +27,86 @@ class Landmark:
     def setHeight(self, height):
         self.height = height
 
-    def setHandedness(self, multi_handedness):
 
-        self.handedness = [None, None]
+    # set relative base coordinates
+    def setBasePoints(self, detections, baseKeyPoint, relativize = True):
 
-        if multi_handedness is None:
+        if detections is None or baseKeyPoint is None:
             return
 
-        # we assume world of max two hands, take most likely
-        # assume hand order corresponds with hand_landmarks order
-        for i, hand in enumerate(multi_handedness):
-            if i > 1: continue
-
-            #print("HAND", hand)
-            self.handedness[i] = hand.classification[0].label
-
-            #for classification in hand.classification:
-                #print("C", classification.label, classification.score)
-                #set something
-
-        print(self.handedness)
-        print("---------------------------")
-        pass
+        for detection in detections:
+            base = mp_face_detection.get_key_point(detection, baseKeyPoint)
+            self.base_x = base.x if relativize else 0
+            self.base_y = base.y if relativize else 0
 
 
-    def setHandLandmarks(self, multi_hand_landmarks):
+    def setHandLandmarks(self, resultsHands):
+
         # 2 hands,
         # 21 landmarks, 3 points (x,y,z) -> 63 flat points
-        self.hand_landmarks = [
-            [-1] * 21 * 3,
-            [-1] * 21 * 3
-        ]
+        self.left_hand_landmarks = [-1] * 21 * 3
+        self.right_hand_landmarks = [-1] * 21 * 3
 
-        if multi_hand_landmarks is None:
+        if resultsHands.multi_hand_landmarks is None:
             return
 
-        #output_csv_hand_landmarks(key, hand_landmarks)
-        #foreach hand i
-        for i, hand_landmarks in enumerate(multi_hand_landmarks):
-            #print(hand_landmarks)
+        for i, hand_landmarks in enumerate(resultsHands.multi_hand_landmarks):
+
+            #yes if classifier labels as two left's will overwrite
+            handedness = resultsHands.multi_handedness[i].classification[0].label #Left, Right
+
+            _hand_landmarks = self.left_hand_landmarks if handedness == "Left" else \
+                self.right_hand_landmarks
+
             for j, landmark in enumerate(hand_landmarks.landmark):
                 k = 3*j
-                self.hand_landmarks[i][k] = landmark.x
-                self.hand_landmarks[i][k+1] = landmark.y
-                self.hand_landmarks[i][k+2] = landmark.z
+                _hand_landmarks[k] = landmark.x - self.base_x
+                _hand_landmarks[k+1] = landmark.y - self.base_y
+                _hand_landmarks[k+2] = landmark.z
 
         #print(self.hand_landmarks)
 
-
-
     def setFaceDetections(self, detections):
-        #6 (x,y) keypoints = 12 points
-        self.face_landmarks = [-1] * 6 * 2
 
         if detections is None:
             return
 
-        #foreach face - assume single face for now
-        #TODO: logic to select nearest face
+        #6 (x,y) keypoints = 12 points
+        self.face_landmarks = [-1] * 6 * 2
+
+        #
+        # TODO: assume single face for now
+        # could use holistic model for singular attached face + hands
+        # e.g. issue if player 2 w/ player 1 hands
+        # but overkill with number of face points in mesh
+        #
         for detection in detections:
+
             #output_csv_face(key, detection.location_data)
             #also detection.location_data.format,
             #detection.location_data.relative_bounding_box
             for i, point in enumerate(detection.location_data.relative_keypoints):
                 j = i*2
-                self.face_landmarks[j] = point.x
-                self.face_landmarks[j+1] = point.y
+                self.face_landmarks[j] = point.x - self.base_x
+                self.face_landmarks[j+1] = point.y - self.base_y
 
         #print(self.face_landmarks)
 
 
     def to_row(self):
         # combined data
-        # consistent left/right hand ordering
-        left = 0 if self.handedness[0] == "Left" else 1
-        right = 1 if left == 0 else 0
 
-        val = [self.width, self.height] + \
-            self.hand_landmarks[left] + self.hand_landmarks[right] + \
+        val = self.left_hand_landmarks + \
+            self.right_hand_landmarks + \
             self.face_landmarks
 
-        print("Landmark", val)
+        #print("Landmark", val)
+        print("Base", self.base_x, self.base_y)
+        print("L", self.left_hand_landmarks)
+        print("R", self.right_hand_landmarks)
         return val
 
 
 if __name__ == "__main__":
     l = Landmark()
     print("MAIN")
-
-
-#resultsHands.multi_handedness
-#NB: this needs to be inverted in selfie mode
-#classification {
-  # index: 1
-  # score: 0.9612538814544678
-  # label: "Right"
-#}
-
-    # if key != -1:
-    #     print("RES", dir(resultsHands))
-    #     print("WOLRD", resultsHands.multi_hand_world_landmarks)
-    #     print("ML", resultsHands.multi_handedness)
