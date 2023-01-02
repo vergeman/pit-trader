61b5405297b482bf791ed5d3c7885fd22d79f72e
vergeman <alan.verga@gmail.com>
2023-01-02 11:42:23 -0600

    add fps control ref via useControl.js hook

--- web/src/Camera.jsx
+++ web/src/Camera.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useRef } from "react";
 import useFaceDetection from "./useFaceDetection.js";
 import useHandsDetection from "./useHandsDetection.js";
+import useControl from "./useControl.js";
 
 export default function Camera(props) {
   //TODO: params isActive on/off given keystroke from above
+  //TODO: verify this doesn't re-render
+  //TODO: promises in onFrame could optimize - Promise.all doesn't work
 
+  const controlRef = useRef(null);
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
 
+  const {control, fpsControl } = useControl(controlRef);
   const faceDetection = useFaceDetection(canvasRef, props.landmarks);
   const hands = useHandsDetection(canvasRef, props.landmarks);
   console.log("FD", faceDetection);
@@ -15,11 +20,16 @@ export default function Camera(props) {
 
   useEffect(() => {
     console.log("useEffect Camera", window.Camera);
+    //console.log("control", control);
 
     const camera = new window.Camera(videoRef.current, {
       onFrame: async () => {
+
+        fpsControl.tick();
+
         await faceDetection.send({ image: videoRef.current });
         await hands.send({ image: videoRef.current });
+
         //await props.landmarks.print()
       },
       width: 1280,
@@ -38,6 +48,10 @@ export default function Camera(props) {
         width="1280px"
         height="720px"
       ></canvas>
+
+      <div ref={controlRef} className="control-panel"
+           style={{position: 'fixed', top: '0'}}>
+      </div>
     </div>
   );
 }
