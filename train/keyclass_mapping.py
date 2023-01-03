59837073635afe298a35c509cb7e09e910d403ba
vergeman <alan.verga@gmail.com>
2023-01-02 18:27:55 -0600

    refactor keyclass_mapping.py to class

--- train/keyclass_mapping.py
+++ train/keyclass_mapping.py
@@ -26,69 +26,76 @@
 #
 # KEYS
 #
+class KeyClassMapping():
 
-# Shift key
-# make sure to change resultant OFFER_ONES, OFFER_TENS
-# elements to represent appropriate keypress.
-OFFER_TOGGLE = "SHIFT"
+  # OFFER_TOGGLE
+  # make sure to change resultant OFFER_ONES, OFFER_TENS
+  # elements to represent appropriate keypress.
+  OFFER_TOGGLE = "SHIFT"
 
-# bids [0..9]
-BID_ONES = list(range(0, 10))
+  # bids [0..9]
+  BID_ONES = list(range(0, 10))
 
-# offers [0..9] - note the keyboard ordering corresponds to 0 first
-OFFER_ONES = [")", "!", "@", "#", "$", "%", "^", "&", "*", "("]
+  # offers [0..9] - note the keyboard ordering corresponds to 0 first
+  OFFER_ONES = [")", "!", "@", "#", "$", "%", "^", "&", "*", "("]
 
-# bids [10, 20...100]
-BID_TENS = [ 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p' ]
+  # bids [10, 20...100]
+  BID_TENS = [ 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p' ]
 
-# offers [10, 20...100]
-OFFER_TENS = [t.upper() for t in BID_TENS]
+  # offers [10, 20...100]
+  OFFER_TENS = [t.upper() for t in BID_TENS]
 
-def _generateMapping(mapping, key, keypress, index, description):
 
-  mapping[key] = {
-    "keypress": keypress,
-    "index": index,
-    "description": description
-  }
+  def __init__(self, OFFER_TOGGLE = "SHIFT"):
+    self.OFFER_TOGGLE = OFFER_TOGGLE
 
-def build():
+    self.mapping = {};
+    self.index = 0;
+    self.build();
 
-  index=0
-  mapping = {}
 
-  # bid ones
-  for keyPress in BID_ONES:
-    value = keyPress
-    _generateMapping(mapping, str(keyPress), str(keyPress), index, f"{value} bid")
-    index = index+1
+  def _generateMapping(self, mapping, key, keypress, index, description):
 
-  # offer ones
-  for i, keyPress in enumerate(OFFER_ONES):
-    value = i
-    _generateMapping(mapping, f"{OFFER_TOGGLE}_{i}", keyPress, index, f"offer {value}")
-    index = index+1
+    mapping[key] = {
+      "keypress": keypress,
+      "index": index,
+      "description": description
+    }
 
-  # bid tens
-  for i, keyPress in enumerate(BID_TENS, 1):
-    value = i * 10
-    _generateMapping(mapping, keyPress, keyPress, index, f"{value} bid")
-    index = index+1
 
-  # offer tens
-  for i, keyPress in enumerate(OFFER_TENS, 1):
-    value = i * 10
-    _generateMapping(mapping, f"{OFFER_TOGGLE}_{keyPress.lower()}", keyPress, index, f"offer {value}")
-    index = index+1
+  def build(self):
 
-  # Misc
-  _generateMapping(mapping, "m", "m", index, "execute market")
-  _generateMapping(mapping, f"{OFFER_TOGGLE}_m", "M", index, "execute market")
-  index += 1
+    # bid ones
+    for keyPress in self.BID_ONES:
+      value = keyPress
+      self._generateMapping(self.mapping, str(keyPress), str(keyPress), self.index, f"{value} bid")
+      self.index = self.index+1
 
-  return mapping, index
+    # offer ones
+    for i, keyPress in enumerate(self.OFFER_ONES):
+      value = i
+      self._generateMapping(self.mapping, f"{self.OFFER_TOGGLE}_{i}", keyPress, self.index, f"offer {value}")
+      self.index = self.index+1
+
+    # bid tens
+    for i, keyPress in enumerate(self.BID_TENS, 1):
+      value = i * 10
+      self._generateMapping(self.mapping, keyPress, keyPress, self.index, f"{value} bid")
+      self.index = self.index+1
+
+    # offer tens
+    for i, keyPress in enumerate(self.OFFER_TENS, 1):
+      value = i * 10
+      self._generateMapping(self.mapping, f"{self.OFFER_TOGGLE}_{keyPress.lower()}", keyPress, self.index, f"offer {value}")
+      self.index = self.index+1
+
+    # Misc
+    self._generateMapping(self.mapping, "m", "m", self.index, "execute market")
+    self._generateMapping(self.mapping, f"{self.OFFER_TOGGLE}_m", "M", self.index, "execute market")
+    self.index += 1
+
 
 if __name__ == "__main__":
-  mapping, num_classes = build()
-  print(num_classes)
-  print(mapping)
+  keyClassMapping = KeyClassMapping()
+  print(keyClassMapping.index)
+  print(keyClassMapping.mapping)
