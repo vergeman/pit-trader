7a7fe1e4b91c5d9d460d49ac99ad4dc7a53c6af9
vergeman <alan.verga@gmail.com>
2023-01-12 16:17:54 -0600

    initial simplified example on/off camera behavior

--- web/src/Camera.jsx
+++ web/src/Camera.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import React from "react";
 import useFaceDetection from "./useFaceDetection.js";
 import useHandsDetection from "./useHandsDetection.js";
@@ -9,88 +9,12 @@ function Camera(props) {
   //TODO: promises in onFrame could optimize - Promise.all doesn't work
 
   const controlRef = useRef(null);
-  const videoRef = useRef(null);
-  const canvasRef = useRef(null);
 
   const {control, fpsControl } = useControl(controlRef);
-  const faceDetection = useFaceDetection(canvasRef, props.landmarks);
-  const hands = useHandsDetection(canvasRef, props.landmarks);
   //console.log("FD", faceDetection);
   //console.log("HANDS", hands);
-
-  useEffect(() => {
-    console.log("Camera [useEffect]", window.Camera);
-    //console.log("control", control);
-    let _arg = null;
-
-    const camera = new window.Camera(videoRef.current, {
-      onFrame: async () => {
-
-        if (fpsControl) {
-          fpsControl.tick();
-
-
-          /*
-           * Developing at < 30 fps - presumably there are faster machines but that
-           * would likely entail rethinking input handling / timings.
-           * So we slow it down by throttling a frame.
-           *
-           * NB: conceptually don't want to drop a frame, as that would speed things
-           * up; want consistency.
-           */
-
-          const lastFPS = fpsControl.g.slice(-1);  //most recent fps rate
-          if ( lastFPS > 30) {
-            await new Promise(resolve => setTimeout(resolve, 1/lastFPS * 1000) );
-          }
-        }
-
-        if (faceDetection) {
-          await faceDetection.send({ image: videoRef.current });
-        }
-
-        if (hands) {
-          await hands.send({ image: videoRef.current });
-        }
-
-        if (props.classifier) {
-
-          const res = await props.classifier.classify();
-
-          //NOTE: minimize renders? - wait for change
-          //if (res && res.arg !==_arg) {
-          //_arg = res.arg;
-          //fpsControl -i secs
-
-          //at this point only want "valid", filtered results to trigger render
-          props.setGestureData(res);
-          //}
-
-        }
-
-        //await props.landmarks.print()
-      },
-      width: 640,
-      height: 480,
-
-      /*
-      width: 1280,
-      height: 720,
-      */
-    });
-
-    camera.start();
-  }, [props.classifier, faceDetection, hands]);
-
   return (
     <div>
-      <video ref={videoRef} className="input_video"></video>
-      <canvas
-        ref={canvasRef}
-        className="output_canvas"
-        width="1280px"
-        height="720px"
-      ></canvas>
 
       <div ref={controlRef} className="control-panel"
            style={{position: 'fixed', top: '0'}}>
--- web/src/Demo.jsx
+++ web/src/Demo.jsx
@@ -1,27 +1,44 @@
-import { useEffect, useReducer, useState } from "react";
+import { forwardRef, useRef, useEffect, useReducer, useState } from "react";
 import { Link } from "react-router-dom";
 import Landmarks from "./Landmarks.js";
 import Classifier from "./Classifier.js";
 import Camera from "./Camera.jsx";
 import GesturesPanel from './GesturesPanel.jsx';
 
-export default function Demo() {
-
+export default function Demo(props, ref) {
+  const videoRef = useRef(null);
   const [gestureData, setGestureData] = useState(null);
   const [landmarks, setLandmarks] = useState(null);
   const [classifier, setClassifier] = useState(null);
-
 
   useEffect(() => {
     console.log("[Demo.jsx]: useEffect init");
     const landmarks = new Landmarks();
     const classifier = new Classifier(landmarks);
 
+    const camera = new window.Camera(videoRef.current, {
+      onFrame: async () => {
+      },
+      width: 640,
+      height: 480,
+    });
+    //props.setHidden(false);
+    camera.start();
+
     setLandmarks(landmarks);
     setClassifier(classifier);
 
     classifier.load();
+
+    return () => {
+      console.log("[Demo] cleanup");
+      camera.stop();
+      //props.setHidden(true);
+    }
+
   }, []);
+
+
 
   /*
    * TODO: create HOC
@@ -37,9 +54,16 @@ export default function Demo() {
     <div className="container">
       <Link to="/">Home</Link>
       <h1>Hello World</h1>
-      <Camera landmarks={landmarks} classifier={classifier}
+      <Camera landmarks={landmarks}
+              classifier={classifier}
               setGestureData={setGestureData} />
       <GesturesPanel results={gestureData} />
+      <video
+        ref={videoRef}
+        className="input_video"></video>
     </div>
   );
 }
+
+//const forwardedRef = forwardRef(Demo);
+//export default forwardedRef;
