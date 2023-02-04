e596fabf7afd3b1cc7d1ffd9c3de4b8a5796b612
vergeman <alan.verga@gmail.com>
2023-02-04 15:32:36 -0600

    PlayerManager.ts: initial init() to setup NPCs and orders and accompanying tests

--- web/src/player/PlayerManager.test.ts
+++ web/src/player/PlayerManager.test.ts
@@ -23,4 +23,87 @@ describe("PlayerManager", () => {
     expect(Object.keys(pm.players).includes(player_b.id)).toBeTruthy();
     expect(Object.keys(pm.players).length).toBe(2);
   });
+
+  it("init() populates respective players orders in player's and matching engine queues", () => {
+    const me = new MatchingEngine();
+    const ordered = [new Player("a"), new Player("b"), new Player("c")];
+    const pm = new PlayerManager(me, ordered);
+    pm.init(50);
+
+    //ensure player has orders
+    expect(ordered.every((player) => player.orders.length === 2)).toBeTruthy();
+
+    //ensure orders submitted to MatchingEngine
+    for (let player of ordered) {
+      for (let order of player.orders) {
+        expect(
+          me.bids.contains(order) || me.offers.contains(order)
+        ).toBeTruthy();
+      }
+    }
+  });
+
+  it("init() creates list of players and with submitted bid/offer orders with same midpoint price and qtySeed limits", () => {
+    const me = new MatchingEngine();
+    const ordered = [new Player("a"), new Player("b"), new Player("c")];
+    const pm = new PlayerManager(me, ordered);
+    pm.init();
+
+    const midpoints = [];
+    for (let player of ordered) {
+      expect(
+        player.orders.every(
+          (order) => Math.abs(order.qty) <= 5 && Math.abs(order.qty) >= 1
+        )
+      ).toBeTruthy();
+      expect(player.orders.length).toBe(2);
+      const midpoint: number =
+        player.orders.reduce((sum, order) => sum + order.price, 0) / 2;
+      midpoints.push(midpoint);
+    }
+
+    //midpoint prices are all the same
+    let j = midpoints.length - 1;
+    for (let i = 0; i < midpoints.length; i++) {
+      expect(midpoints[i] === midpoints[j]).toBeTruthy();
+      j--;
+    }
+  });
+
+  it("randomzedPlayerList() returns randomly ordered list of players", () => {
+    const me = new MatchingEngine();
+    const player_a = new Player("a");
+    const player_b = new Player("b");
+    const player_c = new Player("c");
+    const player_d = new Player("d");
+
+    let ordered = [player_a, player_b, player_c, player_d];
+    const pm = new PlayerManager(me, ordered);
+
+    let notExact = false;
+    let i = 10;
+    while (i) {
+      const players = pm.getRandomizedPlayerList();
+      if (!players.every((player, i) => player === ordered[i])) {
+        notExact = true;
+      }
+      i--;
+    }
+
+    //"as long as they aren't _all_ the same"
+    expect(notExact).toBeTruthy();
+  });
+
+  it("_generateRandomDelta() generates a number from .1 to .5", () => {
+    const me = new MatchingEngine();
+    const pm = new PlayerManager(me, []);
+    let i = 10;
+
+    while (i) {
+      const d = pm.generateRandomDelta();
+      expect(d).toBeGreaterThanOrEqual(0.1);
+      expect(d).toBeLessThanOrEqual(0.5);
+      i--;
+    }
+  });
 });
--- web/src/player/PlayerManager.ts
+++ web/src/player/PlayerManager.ts
@@ -1,5 +1,6 @@
 import Player from "./Player";
 import MatchingEngine from "../engine/MatchingEngine";
+import Order, { OrderType } from "../engine/Order";
 
 export class PlayerManager {
   private _me: MatchingEngine;
@@ -8,8 +9,6 @@ export class PlayerManager {
   constructor(matchingEngine: MatchingEngine, initPlayers: Player[]) {
     this._me = matchingEngine;
     this._players = {};
-
-    this._me = new MatchingEngine();
     this._setInitPlayers(initPlayers);
   }
 
@@ -25,6 +24,71 @@ export class PlayerManager {
 
   get players(): { [key: string]: Player } {
     return this._players;
+  }
+
+  get me(): MatchingEngine {
+    return this._me;
+  }
+
+  init(priceSeed: number = 100, qtySeed: number = 4) {
+    const randomizedPlayers = this.getRandomizedPlayerList();
+
+    for (let player of randomizedPlayers) {
+      const delta = this.generateRandomDelta();
+      const bidPrice = priceSeed - delta;
+      const offerPrice = priceSeed + delta;
+
+      //NB: qtySeed + 1 since 0 is valid number in random range, but invalid
+      //quantity
+      const bidQty = Math.floor(Math.random() * qtySeed + 1);
+      const offerQty = -Math.floor(Math.random() * qtySeed + 1);
+
+      const bidOrder = new Order(player.id, OrderType.Limit, bidQty, bidPrice);
+      const offerOrder = new Order(
+        player.id,
+        OrderType.Limit,
+        offerQty,
+        offerPrice
+      );
+
+      player.addOrder(bidOrder);
+      player.addOrder(offerOrder);
+
+      this._me.process(bidOrder);
+      this._me.process(offerOrder);
+    }
+
+  }
+
+  getRandomizedPlayerList(): Player[] {
+    const players = Object.values(this._players);
+    let num = players.length;
+    let indexes = [...Array(num).keys()]; //[0 to num]
+
+    //shuffle indices
+    while (num) {
+      const idx = Math.floor(Math.random() * num);
+
+      let temp = indexes[num - 1];
+      indexes[num - 1] = indexes[idx];
+      indexes[idx] = temp;
+      num -= 1;
+    }
+
+    //return random copy
+    //(unsure if its ok to shuffle this._players directly)
+    const randomizedPlayers = [];
+    for (let i = 0; i < indexes.length; i++) {
+      const idx = indexes[i];
+      randomizedPlayers.push(players[idx]);
+    }
+
+    return randomizedPlayers;
+  }
+
+  //random digit .1-.5
+  generateRandomDelta(max: number = 4): number {
+    return (Math.floor(Math.random() * max) + 1) / 10;
   }
 }
 
