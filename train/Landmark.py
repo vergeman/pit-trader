8726c6cde9fe13a117ff852da3356b09a3fdcdfd
Alan Verga <alan.verga@gmail.com>
2023-02-15 21:19:30 -0600

    Topic/landmark angles (#19)
    * Landmark angles and cross product initial experiment for additional features
    
    * LandmarkDataset.py: change input_size() to method so Transforms can dynamically add derivative data
    
    * Landmark.py: setPalmOrientation() and setFingersOpen() add boolean arrays to csv output
    
    * Landmark.py: separate left/right palm orientation; minor refactor and save csv output as ints
    
    * ignore .gittoken for https repo push
    
    * update model with expanded palm, angles data
    
    * reformat .py to 4 spaces

--- train/Landmark.py
+++ train/Landmark.py
@@ -2,8 +2,13 @@
 # then build concatenates those
 # we can test what seems to be better for a classifier
 # e.g. each type of data -- doesn't it all get ocncatenated anyway?
+from types import NoneType
+import numpy as np
 import mediapipe as mp
+
 mp_face_detection = mp.solutions.face_detection
+HandLandmark = mp.solutions.holistic.HandLandmark
+
 
 class Landmark:
 
@@ -11,6 +16,10 @@ class Landmark:
         self.left_hand_landmarks = []
         self.right_hand_landmarks = []
         self.face_landmarks = []
+        self.left_palm_orientation = []
+        self.right_palm_orientation = []
+        self.left_fingers_opens = []
+        self.right_fingers_opens = []
 
         # relative points
         self.base_x = 0
@@ -18,7 +27,7 @@ class Landmark:
 
         # default opencv width height
         self.width = 640
-        self.height= 480
+        self.height = 480
         print("__INIT__")
 
     def setWidth(self, width):
@@ -27,9 +36,8 @@ class Landmark:
     def setHeight(self, height):
         self.height = height
 
-
     # set relative base coordinates
-    def setBasePoints(self, detections, baseKeyPoint, relativize = True):
+    def setBasePoints(self, detections, baseKeyPoint, relativize=True):
 
         if detections is None or baseKeyPoint is None:
             return
@@ -38,7 +46,6 @@ class Landmark:
             base = mp_face_detection.get_key_point(detection, baseKeyPoint)
             self.base_x = base.x if relativize else 0
             self.base_y = base.y if relativize else 0
-
 
     def setHandLandmarks(self, resultsHands):
 
@@ -54,18 +61,116 @@ class Landmark:
         for i, hand_landmarks in enumerate(resultsHands.multi_hand_landmarks):
 
             #yes if classifier labels as two left's will overwrite
-            handedness = resultsHands.multi_handedness[i].classification[0].label #Left, Right
+            handedness = resultsHands.multi_handedness[i].classification[
+                0].label  #Left, Right
 
             _hand_landmarks = self.left_hand_landmarks if handedness == "Left" else \
                 self.right_hand_landmarks
 
             for j, landmark in enumerate(hand_landmarks.landmark):
-                k = 3*j
+                k = 3 * j
                 _hand_landmarks[k] = landmark.x - self.base_x
-                _hand_landmarks[k+1] = landmark.y - self.base_y
-                _hand_landmarks[k+2] = landmark.z
+                _hand_landmarks[k + 1] = landmark.y - self.base_y
+                _hand_landmarks[k + 2] = landmark.z
 
-        #print(self.hand_landmarks)
+    def get_tuple(self, arr, index):
+        return (arr[index].x, arr[index].y, arr[index].z)
+
+    def setPalmOrientation(self, resultsHands):
+
+        self.left_palm_orientation = [-1]
+        self.right_palm_orientation = [-1]
+
+        if resultsHands.multi_hand_landmarks is None:
+            return
+
+        for i, hand_landmarks in enumerate(resultsHands.multi_hand_landmarks):
+
+            pt0 = self.get_tuple(hand_landmarks.landmark, 0)
+            pt5 = self.get_tuple(hand_landmarks.landmark, 5)
+            pt17 = self.get_tuple(hand_landmarks.landmark, 17)
+            u = np.subtract(pt17, pt0)
+            v = np.subtract(pt17, pt5)
+            direction = np.cross(u, v)
+            direction /= np.linalg.norm(direction)
+
+            #z-value invert for left hand
+            handedness = resultsHands.multi_handedness[i].classification[
+                0].label  #Left, Right
+            palm_orientation = self.left_palm_orientation if handedness == "Left" else self.right_palm_orientation
+            c = -1 if handedness == "Left" else 1
+            palm_orientation[0] = int(c * direction[-1] > 0)
+            print("PALM FACING" if c * direction[-1] > 0 else "PALM BEHIND")
+
+    def setFingersOpen(self, resultsHands):
+
+        self.left_fingers_opens = [-1] * 5
+        self.right_fingers_opens = [-1] * 5
+
+        if resultsHands.multi_hand_landmarks is None:
+            return
+
+        for i, hand_landmarks in enumerate(resultsHands.multi_hand_landmarks):
+
+            handedness = resultsHands.multi_handedness[i].classification[
+                0].label  #Left, Right
+            fingers_opens = self.left_fingers_opens if handedness == "Left" else self.right_fingers_opens
+
+            # THUMB
+            # why cross product intuition
+            # https://upload.wikimedia.org/wikipedia/commons/a/aa/Cross_product_animation.gif
+            #
+            # when thumb is open vs thumb closed, the relative placement of
+            # vector: point 4, point 3 at each state is akin to red being on
+            # either side of the blue (closed is to "left", open to the "right" of
+            # the other vector.)
+            #
+            # Shown in animation resulting cross product z-value (green vector)
+            # toggle from negative to positive (open to closed)
+            #
+            # NB: also have to accommodate buy/sell and left/right reversals as
+            # well (hence the 'c')
+            p1 = self.get_tuple(hand_landmarks.landmark,
+                                HandLandmark.THUMB_CMC)
+            p3 = self.get_tuple(hand_landmarks.landmark, HandLandmark.THUMB_IP)
+            p4 = self.get_tuple(hand_landmarks.landmark,
+                                HandLandmark.THUMB_TIP)
+            u = np.subtract(p4, p1)
+            v = np.subtract(p4, p3)
+
+            direction = np.cross(u, v)
+            direction /= np.linalg.norm(direction)
+
+            c = -1 if handedness == "Left" else 1
+            palm_orientation = self.left_palm_orientation if handedness == "Left" else self.right_palm_orientation
+            c = c if palm_orientation[0] > 0 else -c
+
+            fingers_opens[0] = int(c * direction[-1] < 0)
+            #print("T UV", u, v, np.subtract(u, v))
+            print("T", "OPEN" if c * direction[-1] < 0 else "CLOSED",
+                  c * direction[-1])
+
+            # FINGERS
+            FINGER_ANGLE = .3
+
+            for finger in range(0, 4):
+                pt1 = self.get_tuple(
+                    hand_landmarks.landmark,
+                    HandLandmark.INDEX_FINGER_MCP + (finger * 4))
+                pt3 = self.get_tuple(
+                    hand_landmarks.landmark,
+                    HandLandmark.INDEX_FINGER_DIP + (finger * 4))
+                pt4 = self.get_tuple(
+                    hand_landmarks.landmark,
+                    HandLandmark.INDEX_FINGER_TIP + (finger * 4))
+
+                u = np.subtract(pt4, pt1)
+                v = np.subtract(pt4, pt3)
+                uv = np.dot(u, v) / np.linalg.norm(u) / np.linalg.norm(v)
+                angle = np.arccos(np.clip(uv, -1, 1))
+                print(finger, "OPEN" if angle < FINGER_ANGLE else "CLOSED",
+                      angle)
+                fingers_opens[finger + 1] = int(angle < FINGER_ANGLE)
 
     def setFaceDetections(self, detections):
 
@@ -83,30 +188,39 @@ class Landmark:
         #
         for detection in detections:
 
-          #output_csv_face(key, detection.location_data)
-          #also detection.location_data.format,
-          #detection.location_data.relative_bounding_box
-          #print(self.base_x, self.base_y, detection.location_data)
-          for i, point in enumerate(detection.location_data.relative_keypoints):
-            j = i*2
-            self.face_landmarks[j] = point.x - self.base_x
-            self.face_landmarks[j+1] = point.y - self.base_y
+            #output_csv_face(key, detection.location_data)
+            #also detection.location_data.format,
+            #detection.location_data.relative_bounding_box
+            #print(self.base_x, self.base_y, detection.location_data)
+            for i, point in enumerate(
+                    detection.location_data.relative_keypoints):
+                j = i * 2
+                self.face_landmarks[j] = point.x - self.base_x
+                self.face_landmarks[j + 1] = point.y - self.base_y
 
         #print(self.face_landmarks)
 
-
     def to_row(self):
         # combined data
-
+        print("L", self.left_fingers_opens)
+        print("R", self.right_fingers_opens)
         val = self.left_hand_landmarks + \
             self.right_hand_landmarks + \
-            self.face_landmarks
+            self.face_landmarks + \
+            self.left_palm_orientation + \
+            self.right_palm_orientation + \
+            self.left_fingers_opens + \
+            self.right_fingers_opens
 
         #print("Landmark", val)
         print("Base", self.base_x, self.base_y)
         print("Face", self.face_landmarks)
         print("L", self.left_hand_landmarks)
         print("R", self.right_hand_landmarks)
+        print("L Palm", self.left_palm_orientation)  #1
+        print("R Palm", self.right_palm_orientation)  #1
+        print("L Open", self.left_fingers_opens)  #5
+        print("R Open", self.right_fingers_opens)  #5
         return val
 
 
--- train/webcam_trainer.py
+++ train/webcam_trainer.py
@@ -10,37 +10,43 @@ import cv2
 import mediapipe as mp
 import csv
 
+
 def output_csv_all(data_path, keyMapVal, landmark):
 
-  if keyMapVal is None: return
+    if keyMapVal is None: return
 
-  _file = keyMapVal.get('filename', None)
-  if (not _file): return
+    _file = keyMapVal.get('filename', None)
+    if (not _file): return
 
-  csv_file = f"{data_path}/{_file}"
+    csv_file = f"{data_path}/{_file}"
 
-  with open(csv_file, 'a', newline="") as _file:
-    writer = csv.writer(_file)
+    with open(csv_file, 'a', newline="") as _file:
+        writer = csv.writer(_file)
 
-    # flattened size: see Landmark.py
-    # label class (keyMapVal.index) -> 1
-    # Hand: 21 points, each with 3 (x,y,z) coordinates
-    # Face: 6 points, each with 2 (x,y) coordinates
-    # Left -> 63, Right -> 63, face -> 12
-    # label + 2*hands + face
-    # 1 + 2*63 + 12 = 1 + 126 + 12 = 139
-    print(keyMapVal)
+        # flattened size: see Landmark.py
+        # label class (keyMapVal.index) -> 1
+        # Hand: 21 points, each with 3 (x,y,z) coordinates
+        # Face: 6 points, each with 2 (x,y) coordinates
+        # Left -> 63, Right -> 63, face -> 12
+        # Palm -> 1
+        # Left Fingers: 5 (thumb + 4 fingers)
+        # Right Fingers: 5 (thumb + 4 fingers)
+        #
+        # label + 2*hands + face + palm + 2*fingers
+        # 1 + 2*63 + 12 + 1 + 10 =
+        # 1 + 126 + 12 + 1 + 10 = 150
+        print(keyMapVal)
 
-    # NB: class index will be mapped and generated at DataLoader.
-    # human label is indicated by filename (see KeyClassMapping.py)
-    # (label no longer part of internal csv data)
-    row = landmark.to_row()
-    writer.writerow(row)
-  _file.close()
+        # NB: class index will be mapped and generated at DataLoader.
+        # human label is indicated by filename (see KeyClassMapping.py)
+        # (label no longer part of internal csv data)
+        row = landmark.to_row()
+        writer.writerow(row)
+    _file.close()
 
 
 cap = cv2.VideoCapture(0)
-print("Webcam Open:", cap.isOpened() )
+print("Webcam Open:", cap.isOpened())
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -53,117 +59,116 @@ keyClassMapping = KeyClassMapping()
 meta = Meta(f"{data_path}/meta.json")
 meta.load()
 
-
 with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands,\
-mp_face_detection.FaceDetection(
-    model_selection=0, min_detection_confidence=0.5) as face_detection:
+        model_complexity=0,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands,\
+        mp_face_detection.FaceDetection(
+            model_selection=0, min_detection_confidence=0.5) as face_detection:
 
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            # If loading a video, use 'break' instead of 'continue'.
+            continue
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    # need to convert opencv (BGR) to RGB for media hands process
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        # need to convert opencv (BGR) to RGB for media hands process
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # hands orientation assumes input is 'selfie mode' (flip)
-    # image needs to be flipped prior to feeding it for processing
-    # for proper hand label
-    image = cv2.flip(image, 1)
+        # hands orientation assumes input is 'selfie mode' (flip)
+        # image needs to be flipped prior to feeding it for processing
+        # for proper hand label
+        image = cv2.flip(image, 1)
 
-    resultsHands = hands.process(image)
-    resultsFace = face_detection.process(image)
-    image.flags.writeable = True
-    # need to convert back to rgb for proper display
-    # convert to use lib, convert back to display
-    # want to convert before drawing landmarks or else landmarks
-    # also get converted (unintended)
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        resultsHands = hands.process(image)
+        resultsFace = face_detection.process(image)
+        image.flags.writeable = True
+        # need to convert back to rgb for proper display
+        # convert to use lib, convert back to display
+        # want to convert before drawing landmarks or else landmarks
+        # also get converted (unintended)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-    # KeyPress input
-    # will wait for N ms or until key is pressed (whichever sooner)
-    # NB: large values hang input loop
-    key = cv2.waitKey(5)
+        # KeyPress input
+        # will wait for N ms or until key is pressed (whichever sooner)
+        # NB: large values hang input loop
+        key = cv2.waitKey(5)
 
-    keyVal = keyClassMapping.getKeyVal(key)
+        keyVal = keyClassMapping.getKeyVal(key)
 
-    #
-    # Draw the hand annotations on the image.
-    # assume multiple hands per frame is possible
-    #
-    # print("---------------------------")
+        #
+        # Draw the hand annotations on the image.
+        # assume multiple hands per frame is possible
+        #
+        # print("---------------------------")
 
-    # Handedness
-    # assumes selfie mode
-    landmark.setWidth( cap.get(cv2.CAP_PROP_FRAME_WIDTH) )
-    landmark.setHeight( cap.get(cv2.CAP_PROP_FRAME_HEIGHT) )
+        # Handedness
+        # assumes selfie mode
+        landmark.setWidth(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        landmark.setHeight(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    # set and re-orient coords around relative base point
-    landmark.setBasePoints(resultsFace.detections,
+        # set and re-orient coords around relative base point
+        landmark.setBasePoints(resultsFace.detections,
                                mp_face_detection.FaceKeyPoint.NOSE_TIP)
 
-    landmark.setHandLandmarks(resultsHands)
+        landmark.setHandLandmarks(resultsHands)
 
-    landmark.setFaceDetections(resultsFace.detections)
+        landmark.setPalmOrientation(resultsHands)
 
-    output_csv_all(data_path, keyVal, landmark)
+        landmark.setFingersOpen(resultsHands)
 
-    if keyVal:
-      meta.update(keyVal.get("filename", None), keyVal)
+        landmark.setFaceDetections(resultsFace.detections)
 
-    #
-    # DRAW
-    #
-    if resultsHands.multi_hand_landmarks:
+        output_csv_all(data_path, keyVal, landmark)
 
-      for hand_landmarks in resultsHands.multi_hand_landmarks:
+        if keyVal:
+            meta.update(keyVal.get("filename", None), keyVal)
 
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-
-    #
-    # draw face detection
-    # loop can detect multiple faces per frame
-    #
-    if resultsFace.detections:
-
-      for detection in resultsFace.detections:
-
-        mp_drawing.draw_detection(image, detection)
-
-        #output_csv_face(key, detection.location_data)
-        # detection.location_data.relative_bounding_box
-        # { xmin, ymin, width,height }
         #
-        # detection.location_data.relative_keypoints
-        # len 6 array of x,y pairs normalized points (see docs)
-        # [ {x,y}, {x,y}...]
+        # DRAW
+        #
+        if resultsHands.multi_hand_landmarks:
 
+            for hand_landmarks in resultsHands.multi_hand_landmarks:
 
-    # resize input
-    # Flip the image horizontally for a selfie-view display.
+                mp_drawing.draw_landmarks(
+                    image, hand_landmarks, mp_hands.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
 
-    # are landmarks normalized, independent of webcam dims?
-    # image = cv2.resize(image, (800, 400))
+        #
+        # draw face detection
+        # loop can detect multiple faces per frame
+        #
+        if resultsFace.detections:
 
-    # finalize
-    # csv_write(landmark)
-    cv2.imshow('MediaPipe Hands + Face', image)
+            for detection in resultsFace.detections:
 
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
+                mp_drawing.draw_detection(image, detection)
 
+                #output_csv_face(key, detection.location_data)
+                # detection.location_data.relative_bounding_box
+                # { xmin, ymin, width,height }
+                #
+                # detection.location_data.relative_keypoints
+                # len 6 array of x,y pairs normalized points (see docs)
+                # [ {x,y}, {x,y}...]
+
+                # resize input
+                # Flip the image horizontally for a selfie-view display.
+
+                # are landmarks normalized, independent of webcam dims?
+                # image = cv2.resize(image, (800, 400))
+
+                # finalize
+                # csv_write(landmark)
+            cv2.imshow('MediaPipe Hands + Face', image)
+
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
 
 cap.release()
