d676162d65d48d433d9188514559c150ed9b4559
vergeman <alan.verga@gmail.com>
2023-02-21 17:10:27 -0600

    Landmark.js: add setPalmOrientation(), resetPalmOrientations(); and accompanying tests for derived landmark data

--- web/src/Landmarks.js
+++ web/src/Landmarks.js
@@ -1,8 +1,13 @@
+import * as glMatrix from "gl-matrix";
+glMatrix.glMatrix.ARRAY_TYPE = Float64Array;
+
 export default class Landmarks {
   constructor() {
     //data
-    this.handLandmarks = new Array(126).fill(-1);
-    this.faceLandmarks = new Array(12).fill(-1);
+    this.handLandmarks = new Array(126).fill(-1); //x,y,z
+    this.faceLandmarks = new Array(12).fill(-1); //x,y
+    this.palmOrientations = new Array(2).fill(-1); //t|f
+    this.fingersOpens = new Array(10).fill(-1); //t|f * 5 * 2
 
     this.base_x = 0;
     this.base_y = 0;
@@ -13,11 +18,10 @@ export default class Landmarks {
   }
 
   setHandLandmarks(hand, landmarks) {
-
     let base = 0;
 
     if (hand === "Right") {
-      base = 63;
+      base = this.handLandmarks.length / 2;
     }
 
     for (let k = 0; k < landmarks.length; k++) {
@@ -26,6 +30,48 @@ export default class Landmarks {
       this.handLandmarks[base + 3 * k + 2] = landmarks[k].z;
     }
   }
+
+  getVec3LandmarksIndex(landmarks, index) {
+    return glMatrix.vec3.fromValues(
+      landmarks[index].x - this.base_x,
+      landmarks[index].y - this.base_y,
+      landmarks[index].z
+    );
+  }
+
+  setPalmOrientations(hand, landmarks) {
+    let idx = Number(hand === "Right");
+    const u = glMatrix.vec3.create();
+    const v = glMatrix.vec3.create();
+    const direction = glMatrix.vec3.create();
+    const norm_direction = glMatrix.vec3.create();
+
+    const pt0 = this.getVec3LandmarksIndex(landmarks, 0);
+    const pt5 = this.getVec3LandmarksIndex(landmarks, 5);
+    const pt17 = this.getVec3LandmarksIndex(landmarks, 17);
+
+    glMatrix.vec3.subtract(u, pt17, pt0);
+    glMatrix.vec3.subtract(v, pt17, pt5);
+    glMatrix.vec3.cross(direction, u, v);
+    glMatrix.vec3.normalize(norm_direction, direction);
+
+    let c = 1;
+    if (hand === "Left") {
+      c = -1;
+    }
+
+    const orientation = c * norm_direction.at(-1) > 0;
+    this.palmOrientations[idx] = Number(orientation);
+    //console.log( orientation ? "PALM_FACING" : "PALM BEHIND");
+    return this.palmOrientations;
+  }
+
+  resetPalmOrientations() {
+    this.palmOrientations = new Array(2).fill(-1);
+  }
+
+  //TODO
+  setFingersOpen(hand, palmOrientations, landmarks) {}
 
   resetFaceLandmarks() {
     this.faceLandmarks = new Array(12).fill(-1);
@@ -47,6 +93,6 @@ export default class Landmarks {
   }
 
   print() {
-    console.log(JSON.stringify( this.get()) );
+    console.log(JSON.stringify(this.get()));
   }
 }
