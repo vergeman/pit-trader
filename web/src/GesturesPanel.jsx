594cd417ca71d3e32fe74d5133393a20d9a5ed0d
vergeman <alan.verga@gmail.com>
2023-02-24 15:09:52 -0600

    GesturesPanel.jsx: revise display ordering

--- web/src/GesturesPanel.jsx
+++ web/src/GesturesPanel.jsx
@@ -29,6 +29,8 @@ export default function GesturesPanel(props) {
 
   const wrapStyle = {
     height: "300px",
+    display: "flex",
+    justifyContent: "center",
   };
 
   const tableStyle = {
@@ -41,97 +43,104 @@ export default function GesturesPanel(props) {
   };
 
   return (
-    <div style={wrapStyle} className="gestures-live gestures-prob">
-      {/* Current Gesture (probs below might not exceed threshold) */}
-      <table style={tableStyle}>
-        <caption>Gesture Current</caption>
-        <thead>
-          <tr>
-            <th style={tableHeaderStyle}>Type&nbsp;&nbsp;</th>
-            <th style={tableHeaderStyle}>Action</th>
-            <th style={tableHeaderStyle}>Value&nbsp;</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{results && results.gesture.type}</td>
-            <td>{results && results.gesture.action}</td>
-            <td>{results && results.gesture.value}</td>
-          </tr>
-        </tbody>
-      </table>
-
-      {/* GestureDecision State: Partial state, waiting for all gestures to
+    <div style={wrapStyle}>
+      <div className="gestures-current gestures-decision gestures-prob">
+        {/* GestureDecision State: Partial state, waiting for all gestures to
           build action */}
-      <table style={tableStyle}>
-        <caption>Gesture Decision</caption>
-        <thead>
-          <tr>
-            <th style={tableHeaderStyle}>Action</th>
-            <th style={tableHeaderStyle}>Qty&nbsp;&nbsp;&nbsp;</th>
-            <th style={tableHeaderStyle}>Price&nbsp;</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{props.gestureDecision.action}</td>
-            <td>{props.gestureDecision.qty}</td>
-            <td>{props.gestureDecision.price}</td>
-          </tr>
-        </tbody>
-      </table>
+        <table style={tableStyle}>
+          <caption>Gesture Decision</caption>
+          <thead>
+            <tr>
+              <th style={tableHeaderStyle}>Action</th>
+              <th style={tableHeaderStyle}>Qty&nbsp;&nbsp;&nbsp;</th>
+              <th style={tableHeaderStyle}>Price&nbsp;</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{props.gestureDecision.action}</td>
+              <td>{props.gestureDecision.qty}</td>
+              <td>{props.gestureDecision.price}</td>
+            </tr>
+          </tbody>
+        </table>
 
-      {/* Live Gesture */}
-      <table style={tableStyle}>
-        <caption>Gesture Probabilities</caption>
-        <thead>
-          <tr>
-            <th style={tableHeaderStyle}>Index&nbsp;</th>
-            <th style={tableHeaderStyle}>Type&nbsp;&nbsp;</th>
-            <th style={tableHeaderStyle}>Action</th>
-            <th style={tableHeaderStyle}>Value&nbsp;</th>
-            <th style={tableHeaderStyle}>Prob&nbsp;&nbsp;</th>
-          </tr>
-        </thead>
-        <tbody>
-          {metas.map((element, i) => {
-            return (
-              <tr key={`gesture-${i}`}>
-                <td key={`gesture-index-${i}`}>{element.index}</td>
-                <td key={`gesture-type-${i}`}>{element.gestureType}</td>
-                <td key={`gesture-action-${i}`}>{element.action}</td>
-                <td key={`gesture-value-${i}`}>{element.value}</td>
-                <td key={`gesture-prob-${i}`}>{element.prob}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+        {/* Current Gesture (probs below might not exceed threshold) */}
+        <table style={tableStyle}>
+          <caption>Gesture Current</caption>
+          <thead>
+            <tr>
+              <th style={tableHeaderStyle}>Type&nbsp;&nbsp;</th>
+              <th style={tableHeaderStyle}>Action</th>
+              <th style={tableHeaderStyle}>Value&nbsp;</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{results && results.gesture.type}</td>
+              <td>{results && results.gesture.action}</td>
+              <td>{results && results.gesture.value}</td>
+            </tr>
+          </tbody>
+        </table>
 
-      {/* ok can't use orders - orders for cancel, will modify the order but action will be out of sequence */}
-      {/* sort by timestamp */}
+        {/* Live Gesture */}
+        <table style={tableStyle}>
+          <caption>Gesture Probabilities</caption>
+          <thead>
+            <tr>
+              <th style={tableHeaderStyle}>Index&nbsp;</th>
+              <th style={tableHeaderStyle}>Type&nbsp;&nbsp;</th>
+              <th style={tableHeaderStyle}>Action</th>
+              <th style={tableHeaderStyle}>Value&nbsp;</th>
+              <th style={tableHeaderStyle}>Prob&nbsp;&nbsp;</th>
+            </tr>
+          </thead>
+          <tbody>
+            {metas.map((element, i) => {
+              return (
+                <tr key={`gesture-${i}`}>
+                  <td key={`gesture-index-${i}`}>{element.index}</td>
+                  <td key={`gesture-type-${i}`}>{element.gestureType}</td>
+                  <td key={`gesture-action-${i}`}>{element.action}</td>
+                  <td key={`gesture-value-${i}`}>{element.value}</td>
+                  <td key={`gesture-prob-${i}`}>{element.prob}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+
       {/* Past Orders */}
-      <table style={tableStyle}>
-        <caption>Past Gestures</caption>
-        <thead>
-          <tr>
-            <th style={tableHeaderStyle}>Type&nbsp;&nbsp;</th>
-            <th style={tableHeaderStyle}>Qty</th>
-            <th style={tableHeaderStyle}>Value&nbsp;</th>
-          </tr>
-        </thead>
-        <tbody>
-          {records.map((record, i) => {
-            return (
-              <tr key={`record-${i}`}>
-                <td key={`record-action-${i}`}>{record.action}</td>
-                <td key={`record-qty-${i}`}>{record.qty}</td>
-                <td key={`record-price-${i}`}>{record.price}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="gestures-records">
+        <table style={tableStyle}>
+          <caption>Past Gestures</caption>
+          <thead>
+            <tr>
+              <th style={tableHeaderStyle}>Time&nbsp;&nbsp;</th>
+              <th style={tableHeaderStyle}>Type&nbsp;&nbsp;</th>
+              <th style={tableHeaderStyle}>Qty</th>
+              <th style={tableHeaderStyle}>Value&nbsp;</th>
+            </tr>
+          </thead>
+          <tbody>
+            {records.map((record, i) => {
+              const d = new Date(record.timestamp);
+              return (
+                <tr key={`record-${i}`}>
+                  <td key={`record-timestamp-${i}`} style={{minWidth: '100px'}}>
+                    {d.toLocaleTimeString()}
+                  </td>
+                  <td key={`record-action-${i}`}>{record.action}</td>
+                  <td key={`record-qty-${i}`}>{record.qty}</td>
+                  <td key={`record-price-${i}`}>{record.price}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
