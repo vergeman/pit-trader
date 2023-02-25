c0824fa66bac7dcc52460012376a9d8ec64e07e9
vergeman <alan.verga@gmail.com>
2023-02-24 19:42:52 -0600

    MatchingEngine add getSums() helper to build matching engine qty summation vs price display

--- web/src/MatchingEngineView.jsx
+++ web/src/MatchingEngineView.jsx
@@ -7,10 +7,14 @@ export default function MatchingEngineView(props) {
 
   if (!props.me) return null;
 
+  const { bidMap, bidPriceLabel } = props.me.getSumBids(props.player.id);
+  const { offerMap, offerPriceLabel } = props.me.getSumOffers(props.player.id);
+
+  /* Styles */
   const gridWrapperStyle = {
     display: "flex",
     justifyContent: "center",
-    width: "400px"
+    width: "400px",
   };
   const gridWrapStyle = {
     width: "700px",
@@ -26,7 +30,6 @@ export default function MatchingEngineView(props) {
     alignSelf: "flex-end",
     textAlign: "left",
   };
-
   const gridBidsStyle = {
     display: "flex",
     flexDirection: "column",
@@ -34,72 +37,65 @@ export default function MatchingEngineView(props) {
     textAlign: "right",
   };
 
-  const offers = props.me.offers.toArray().sort( (a,b) => {
-    if (a.price - b.price == 0) return a.timestamp > b.timestamp
-    return a.price > b.price
-  });
-
-  const bids = props.me.bids.toArray().sort( (a,b) => {
-    if (a.price - b.price === 0) return a.timestamp > b.timestamp
-    return a.price < b.price
-  });
-
   return (
     <div className="grid-wrapper" style={gridWrapperStyle}>
-
       <div className="grid-wrap" style={gridWrapStyle}>
         <p>Market View</p>
         <div className="grid" style={gridStyle}>
           <div></div>
           <div className="grid-offers" style={gridOffersStyle}>
-            {
-              offers.map( offer => {
+            {Array.from(offerMap).map(([price, qty]) => {
+              let gridElementStyle = {};
+              if (offerPriceLabel[price]) {
+                gridElementStyle = { background: "yellow" };
+              }
 
-                let gridElementStyle = {};
-                if(offer.player_id == props.player.id) {
-                  gridElementStyle = {background: "yellow"}
-                }
+              return (
+                <div className="grid-offers-offer" style={gridElementStyle}>
+                  <span>{price}</span>
+                  <span> | </span>
+                  <span>{qty}</span>
+                  <span> | </span>
 
-                return(
-
-                  <div className="grid-offers-offer" style={gridElementStyle}>
-
-                    <span>{offer.price}</span>
-                    <span> | </span>
-                    <span>{offer.qty}</span>
-                    <span> | </span>
-                    <span>{(new Date(offer.timestamp)).toLocaleTimeString()}</span>
-                  </div>
-                )
-              })
-            }
+                  {/*
+                <span>{offer.price}</span>
+                <span> | </span>
+                <span>{offer.qty}</span>
+                <span> | </span>
+                <span>{(new Date(offer.timestamp)).toLocaleTimeString()}</span>
+              */}
+                </div>
+              );
+            })}
           </div>
 
           <div className="grid-bids" style={gridBidsStyle}>
-            {
-              bids.map( bid => {
+            {Array.from(bidMap).map(([price, qty]) => {
+              let gridElementStyle = {};
+              if (bidPriceLabel[price]) {
+                gridElementStyle = { background: "yellow" };
+              }
 
-                let gridElementStyle = {};
-                if(bid.player_id == props.player.id) {
-                  gridElementStyle = {background: "yellow"}
-                }
+              return (
+                <div className="grid-bids-bid" style={gridElementStyle}>
+                  <span>{qty}</span>
+                  <span> | </span>
+                  <span>{price}</span>
+                  <span> | </span>
 
-                return(
-                  <div className="grid-bids-bid" style={gridElementStyle}>
-                    <span>{(new Date(bid.timestamp)).toLocaleTimeString()}</span>
-                    <span> | </span>
-                    <span>{bid.qty}</span>
-                    <span> | </span>
-                    <span>{bid.price}</span>
-                  </div>
-                )
-              })
-            }
+                  {/*
+                <span>{(new Date(bid.timestamp)).toLocaleTimeString()}</span>
+                <span> | </span>
+                <span>{bid.qty}</span>
+                <span> | </span>
+                <span>{bid.price}</span>
+              */}
+                </div>
+              );
+            })}
           </div>
 
-
           <div></div>
-
         </div>
       </div>
     </div>
