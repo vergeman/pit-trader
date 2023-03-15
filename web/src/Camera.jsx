34a0968db714f5148db16a9f54d2dd340709f85e
vergeman <alan.verga@gmail.com>
2023-03-15 18:39:48 -0500

    stub out handle resize for video canvas

--- web/src/Camera.jsx
+++ web/src/Camera.jsx
@@ -1,10 +1,13 @@
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 import useFaceDetection from "./useFaceDetection.js";
 import useHandsDetection from "./useHandsDetection.js";
 import useSelfieDetection from "./useSelfieDetection.js";
 import useCamera from "./useCamera.js";
 
 function Camera(props) {
+  const [width, setWidth] = useState("640px");
+  const [height, setHeight] = useState("480px");
+
   const controlRef = useRef(null);
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
@@ -12,10 +15,6 @@ function Camera(props) {
   const faceDetection = useFaceDetection(canvasRef, props.landmarks);
   const handsDetection = useHandsDetection(canvasRef, props.landmarks);
   const selfieDetection = useSelfieDetection(canvasRef, props.landmarks);
-
-  //TODO: handle onResize
-  const width = "640px";
-  const height = "480px";
 
   const camera = useCamera(
     videoRef,
@@ -27,8 +26,30 @@ function Camera(props) {
     props.setGestureData
   );
 
+  const handleResize = (e) => {
+    console.log("resize", e);
+    console.log(window.innerWidth, window.innerHeight);
+    /*
+     * TODO: calc 640 / 480 or device dependent aspect ratio
+     * possible debounce on resize
+     * take min of default dim vs innerWidth/innerHeight aspect ratio
+     * setWidth(), setHeight()
+     */
+  };
+
+  useEffect( () => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const canvasStyle = {
+    width, height
+  };
+
   return (
-    <div className="video-canvas">
+    <div className="video-canvas" style={canvasStyle} onResize={handleResize}>
       <div className="input-output">
         <video ref={videoRef} className="input_video"></video>
         <canvas
