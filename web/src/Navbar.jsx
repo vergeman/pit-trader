c88e6f4d69334194e143a74e465c60c2e34fd03d
vergeman <alan.verga@gmail.com>
2023-03-15 15:14:29 -0500

    Navbar.jsx stub

--- web/src/Navbar.jsx
+++ web/src/Navbar.jsx
@@ -1,10 +1,37 @@
 import { Link } from "react-router-dom";
+import {Container, Nav, Navbar} from "react-bootstrap";
 
-export default function Navbar() {
+export default function NavbarComponent() {
+  const inheritStyle = { color: "inherit", textDecoration: "inherit" };
+
   return (
-    <div className="header">
-      <Link to="/">Home</Link>
-      <h1>Hello World</h1>
-    </div>
+    <Navbar bg="dark" variant="dark" fixed="top">
+      <Container>
+        <Navbar.Brand>
+          {/* TODO: adjust logo */}
+          <img
+            src="https://react-bootstrap.github.io/logo.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
+          <Link to="/" style={inheritStyle}>
+            Pit Trader
+          </Link>
+        </Navbar.Brand>
+
+        {/* Links */}
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/">
+            Home
+          </Nav.Link>
+
+          <Nav.Link as={Link} to="/demo">
+            Demo
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
