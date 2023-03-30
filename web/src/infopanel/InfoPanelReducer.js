47a6f1efb033cb79d6c5fe70b5192ba5f6ac5efa
vergeman <alan.verga@gmail.com>
2023-03-30 16:02:39 -0500

    initial Message enum - template string example with messageReducer

--- web/src/infopanel/InfoPanelReducer.js
+++ web/src/infopanel/InfoPanelReducer.js
@@ -1,3 +1,5 @@
+import Message from "./Message.js";
+
 export function activeTabReducer(activeTab, action) {
   //console.log("[activeTabReducer]", action);
   switch (action.type) {
@@ -10,19 +12,27 @@ export function activeTabReducer(activeTab, action) {
   }
 }
 
+const populateTemplateString = (template, vars) => {
+  for (let i = 1; i <= vars.length; i++) {
+    template = template.replaceAll(`\$\{${i}\}`, vars[i - 1]);
+  }
+  return template;
+};
+
 export function messagesReducer(messages, action) {
+  let text;
   switch (action.type) {
-    case "add":
-      console.log("ADD", action.text);
-      return [
-        {
-          //id?
-          text: action.text,
-        },
-        ...messages,
-      ];
+    case Message.SetPrice:
+      text = populateTemplateString(Message.SetPrice, [action.value.price]);
+
+    case Message.SetQty:
+      text = populateTemplateString(Message.SetQty, [action.value.qty]);
 
     default: {
+      const text = action.text;
+      if (text) {
+        return [{ text }, ...messages];
+      }
       throw new Error("action doesn't exist", action.type);
     }
   }
---
+++ web/src/infopanel/Message.js
@@ -0,0 +1,8 @@
+export const Message = {
+  SetPrice: "Set price: ${1}",
+  SetQty: "Set qty: ${1}",
+  OrderCancel: "Order ${1} cancelled",
+  CancelGesture: "Clearing Order"
+};
+
+export default Message;
