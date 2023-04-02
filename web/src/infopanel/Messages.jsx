ab626428ce3498398a70f86047a8a47f5affce4f
vergeman <alan.verga@gmail.com>
2023-04-01 19:20:59 -0500

    remove extraneous message html / copy; add padding to tab-content

--- web/src/infopanel/Messages.jsx
+++ web/src/infopanel/Messages.jsx
@@ -1,23 +1,15 @@
 /* News / Alert/ Challenge / Message Component */
 import { useInfoPanel } from "./InfoPanelContext.jsx";
-import { Row, Col } from "react-bootstrap";
 
 export default function Messages(props) {
 
   const { messages } = useInfoPanel();
 
   return (
-    <Row>
-      <Col>
-        <div className="d-flex justify-content-center">
-          <div>News Logger says</div>
-          <div>
-            {messages.map((message, i) => {
-              return <div key={`message-${i}`}>{message.text}</div>;
-            })}
-          </div>
-        </div>
-      </Col>
-    </Row>
+    <div>
+      {messages.map((message, i) => {
+        return <div key={`message-${i}`}>{message.text}</div>;
+      })}
+    </div>
   );
 }
