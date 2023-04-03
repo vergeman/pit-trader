8e4b32cf5fa9b7c2467ca9526b066c31d0630a01
vergeman <alan.verga@gmail.com>
2023-04-02 22:30:19 -0500

    MatchingEngineView.jsx: mobile style condensed best bid offer

--- web/src/MatchingEngineView.jsx
+++ web/src/MatchingEngineView.jsx
@@ -19,7 +19,11 @@ export default function MatchingEngineView(props) {
     const priceArr = [price];
     if (price.length < maxLen) {
       for (let i = 0; i < maxLen - price.length; i++) {
-        const spacer = <span key={`spacer-{$i}`}className="invisible">0</span>;
+        const spacer = (
+          <span key={`spacer-{$i}`} className="invisible">
+            0
+          </span>
+        );
         priceArr.unshift(spacer);
       }
     }
@@ -27,30 +31,58 @@ export default function MatchingEngineView(props) {
     return priceArr;
   };
 
+  const bestBidOrder = props.me.bids.peek();
+  const bestOfferOrder = props.me.offers.peek();
+
   return (
-    <Table size="sm" className="text-center caption-top">
-      <thead>
-        <tr>
-          <th>Buy Qty</th>
-          <th>Bid Size</th>
-          <th>Price</th>
-          <th>Offer Size</th>
-          <th>Sell Qty</th>
-        </tr>
-      </thead>
-      <tbody>
-        {prices.map((price, i) => {
-          return (
-            <tr key={price} className={price == lastPrice ? "me-isLast": ""}>
-              <td>{bidMaps.playerOrdersPriceQtyMap.get(Number(price))}</td>
-              <td>{bidMaps.allOrdersPriceQtyMap.get(Number(price))}</td>
-              <td>{renderPrice(price, gridNumMaxLen)}</td>
-              <td>{offerMaps.allOrdersPriceQtyMap.get(Number(price))}</td>
-              <td>{offerMaps.playerOrdersPriceQtyMap.get(Number(price))}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </Table>
+    <>
+      {/* Mobile condensed bid/offer view */}
+      <Table size="sm" className="text-center caption-top d-md-none">
+        <thead>
+          <tr>
+            <th>Qty</th>
+            <th>Bid</th>
+            <th>Offer</th>
+            <th>Qty</th>
+            <th>Last</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{bestBidOrder && bestBidOrder.qty}</td>
+            <td>{bestBidOrder && bestBidOrder.price}</td>
+            <td>{bestOfferOrder && bestOfferOrder.price}</td>
+            <td>{bestOfferOrder && bestOfferOrder.qty}</td>
+            <td>{lastPrice}</td>
+          </tr>
+        </tbody>
+      </Table>
+
+      {/* Desktop 'active trader' view */}
+      <Table size="sm" className="text-center caption-top d-none d-md-table">
+        <thead>
+          <tr>
+            <th>Buy Qty</th>
+            <th>Bid Size</th>
+            <th>Price</th>
+            <th>Offer Size</th>
+            <th>Sell Qty</th>
+          </tr>
+        </thead>
+        <tbody>
+          {prices.map((price, i) => {
+            return (
+              <tr key={price} className={price == lastPrice ? "me-isLast" : ""}>
+                <td>{bidMaps.playerOrdersPriceQtyMap.get(Number(price))}</td>
+                <td>{bidMaps.allOrdersPriceQtyMap.get(Number(price))}</td>
+                <td>{renderPrice(price, gridNumMaxLen)}</td>
+                <td>{offerMaps.allOrdersPriceQtyMap.get(Number(price))}</td>
+                <td>{offerMaps.playerOrdersPriceQtyMap.get(Number(price))}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </>
   );
 }
