bf779663a239a1a234183476ed2a7c02cd1928f8
vergeman <alan.verga@gmail.com>
2023-04-05 12:51:13 -0500

    initial css gestures dash

--- web/src/GesturesCurrent.jsx
+++ web/src/GesturesCurrent.jsx
@@ -2,9 +2,21 @@
 export default function GesturesCurrent(props) {
   if (!props.isDebug) return null;
 
+  const tableStyle = {
+    boxShadow: '0 8px 16px rgba(0, 0, 0, 0.3)',
+    transform: 'translate3d(0, 0, 0)',
+    width: '400px',
+    textAlign: 'center',
+    margin: '1rem auto',
+    marginTop: '-2rem',
+    fontSize: '1rem',
+  };
+
+  const type = props.gesture.type == "Action" ? '' : props.gesture.type;
+  const action = props.gesture.action == "Garbage" ? '' : props.gesture.action;
+
   return (
-    <table className="table table-sm table-bordered caption-top w-100">
-      <caption>Gesture Current</caption>
+    <table className="table table-sm table-bordered caption-top" style={tableStyle}>
       <thead>
         <tr>
           <th>Type&nbsp;&nbsp;</th>
@@ -15,8 +27,8 @@ export default function GesturesCurrent(props) {
       </thead>
       <tbody>
         <tr>
-          <td>{props.gesture.type}</td>
-          <td>{props.gesture.action}</td>
+          <td>{type || "-"}</td>
+          <td>{action}</td>
           <td>{props.gesture.value}</td>
           <td>{props.gesture.value && props.gesture.prob}</td>
         </tr>
