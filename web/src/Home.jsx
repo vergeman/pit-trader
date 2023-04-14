84a3a35bf541469a2c829ca2b25d9694f514860d
Alan Verga <alan.verga@gmail.com>
2023-04-14 18:09:51 -0500

    Topic/home 1 (#38)
    * initial simple splash screen
    
    * Home.jsx: add input component with defaultBadgeBuilder() and onChange handler
    
    * revised styling Badge input for name/high score
    
    * fix deprecated test

--- web/src/Home.jsx
+++ web/src/Home.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Card} from "react-bootstrap";
 import { LoadingInstructions } from "./instructions";
 
 export default function Home() {
+  const defaultBadgeBuilder = () => {
+    const badge = [];
+    const iters = Math.ceil(Math.random()) + 3; //3 or 4
+    for (let i = 0; i < iters; i++) {
+      const charCode = Math.floor(Math.random() * 26 + 97); //0-25 + 97 (a)
+      badge.push(String.fromCharCode(charCode).toUpperCase());
+    }
+    return badge.join("");
+  };
+
+  const changeBadgeInputHandler = (e) => {
+    const val = e.target.value.toUpperCase();
+    setBadge(val);
+  };
+
+  const [defaultBadge, setDefaultBadge] = useState(defaultBadgeBuilder());
+  const [badge, setBadge] = useState("");
+
   return (
     <div>
       <img
@@ -11,18 +30,24 @@ export default function Home() {
         alt="background image pit trading"
       />
       <div className="App-header">
+        <h1 id="home-hero-text">PIT TRADER</h1>
 
-        <h1 class="display-1 fw-bold">
-          Pit Trader
-        </h1>
+        <div id="home-badge">
+          <p className="fs-6 fw-light text-dark">Enter Badge ID</p>
+          <input
+            name="badge"
+            className="fw-light no-border w-100"
+            placeholder={defaultBadge}
+            value={badge}
+            onChange={changeBadgeInputHandler}
+          />
+        </div>
 
-        <Link to="/demo">
-          <Button size="lg" variant="dark">Enter</Button>
+        <Link to={`/demo?badge=${badge || defaultBadge}`}>
+          <Button id="home-hero-cta" size="lg" variant="dark">
+            Enter
+          </Button>
         </Link>
-      </div>
-
-      <div className="d-flex flex-column justify-center align-items-center">
-        <LoadingInstructions />
       </div>
     </div>
   );
