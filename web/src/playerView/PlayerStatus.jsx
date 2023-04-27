91eb118116c84435f4f1cc72e655f6ce0d043823
vergeman <alan.verga@gmail.com>
2023-04-27 11:19:39 -0500

    PlayerStatus.jsx: toggle style red when at warnPositionLimit; minor formatting

--- web/src/playerView/PlayerStatus.jsx
+++ web/src/playerView/PlayerStatus.jsx
@@ -1,7 +1,7 @@
 import PlayerStatusHeaderElement from "./PlayerStatusHeaderElement.jsx";
 
 export default function PlayerStatus(props) {
-  if (!props.player || !props.marketLoop) return null;
+  if (!props.player || !props.marketLoop || !props.riskManager) return null;
 
   //openPosition: held position
   //price is "best" - pnl is based on this price
@@ -10,18 +10,39 @@ export default function PlayerStatus(props) {
   //lastPrice: last traded
   const openPosition = props.player.openPosition();
   const price = props.marketLoop.getPrice();
-  const pnl = Number(props.player.lostPnL || props.player.calcPnL(price)).toFixed(2);
+  const pnl = Number(
+    props.player.lostPnL || props.player.calcPnL(price)
+  ).toFixed(2);
   const avgPrice = props.player.calcDisplayAvgPrice();
   const lastPrice = props.marketLoop.getDisplayLastPrice();
+
+  //calc warning indicator
+  const positions = props.riskManager._calcPositions(props.player);
+  const limitClass =
+    positions.open >= props.riskManager.warnPositionLimit ? "text-danger" : "";
+
   //open, avgPrice -> pnl, price, last
   return (
     <div className="player-status mb-md-2">
       <div className="d-flex flex-wrap justify-content-around p-1">
-        <PlayerStatusHeaderElement label="Name" value={props.player.name} className="d-none d-md-flex"/>
-        <PlayerStatusHeaderElement label="Position" value={openPosition} />
+        <PlayerStatusHeaderElement
+          label="Name"
+          value={props.player.name}
+          className="d-none d-md-flex"
+        />
+        <PlayerStatusHeaderElement
+          labelClassName={limitClass}
+          valueClassName={limitClass}
+          label="Position"
+          value={openPosition}
+        />
         <PlayerStatusHeaderElement label="P&L" value={pnl} />
         <PlayerStatusHeaderElement label="Avg Price" value={avgPrice} />
-        <PlayerStatusHeaderElement label="Last" value={lastPrice && lastPrice.toFixed(1)} className="d-none d-md-flex"/>
+        <PlayerStatusHeaderElement
+          label="Last"
+          value={lastPrice && lastPrice.toFixed(1)}
+          className="d-none d-md-flex"
+        />
       </div>
     </div>
   );
