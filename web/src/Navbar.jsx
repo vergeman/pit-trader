2ca8eec0adc8a52a95b1a150776976f8e0e3d964
vergeman <alan.verga@gmail.com>
2023-04-28 23:07:17 -0500

    Navbar remove stub logos, replace with title font

--- web/src/Navbar.jsx
+++ web/src/Navbar.jsx
@@ -4,30 +4,18 @@ import { Instructions, InstructionModalContainer } from "./instructions";
 
 export default function NavbarComponent() {
   const inheritStyle = { color: "inherit", textDecoration: "inherit" };
-
   return (
     <Navbar bg="dark" variant="dark" fixed="top">
       <Container>
         <Navbar.Brand>
-          {/* TODO: adjust logo */}
-          <img
-            src="https://react-bootstrap.github.io/logo.svg"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-            alt="React Bootstrap logo"
-          />
-          <Link to="/" style={inheritStyle}>
-            Pit Trader
+
+          <Link id="nav-home-logo" style={inheritStyle} to="/" >
+            PIT TRADER
           </Link>
         </Navbar.Brand>
 
         {/* Links */}
         <Nav className="me-auto w-100">
-          <Nav.Link as={Link} to="/">
-            Home
-          </Nav.Link>
-
           <InstructionModalContainer title="Instructions">
             <Instructions />
           </InstructionModalContainer>
