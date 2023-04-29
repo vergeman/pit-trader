efe9cf503a9459ef07dc68353bc58d49e0682db4
vergeman <alan.verga@gmail.com>
2023-04-28 20:37:16 -0500

    GesturesCurrent: remove extranous commented code

--- web/src/GesturesCurrent.jsx
+++ web/src/GesturesCurrent.jsx
@@ -2,26 +2,20 @@
 export default function GesturesCurrent(props) {
   if (!props.isDebug) return null;
 
-  const type = props.gesture.type == "Action" ? '' : props.gesture.type;
-  const action = props.gesture.action == "Garbage" ? '' : props.gesture.action;
+  const type = props.gesture.type == "Action" ? "" : props.gesture.type;
+  const action = props.gesture.action == "Garbage" ? "" : props.gesture.action;
 
   return (
-    <table id="gestureCurrent" className="table table-sm table-bordered mt-md-n4">
+    <table
+      id="gestureCurrent"
+      className="table table-sm table-bordered mt-md-n4"
+    >
       <caption>Gesture</caption>
-      {/* <thead> */}
-      {/*   <tr> */}
-      {/*     <th>Type&nbsp;&nbsp;</th> */}
-      {/*     <th>Action</th> */}
-      {/*     <th>Value&nbsp;</th> */}
-      {/*     {/\* <th>Prob&nbsp;&nbsp;</th> *\/} */}
-      {/*   </tr> */}
-      {/* </thead> */}
       <tbody>
         <tr>
           <td>{type}</td>
           <td>{action || "-"}</td>
-    <td>{props.gesture.value ? Math.abs(props.gesture.value) : null}</td>
-          {/* <td>{props.gesture.value && props.gesture.prob}</td> */}
+          <td>{props.gesture.value ? Math.abs(props.gesture.value) : null}</td>
         </tr>
       </tbody>
     </table>
